6fc50025e569c6951ad50e895830a1d5160eeb46
HARDIK ARORA <122469904+barbaria888@users.noreply.github.com>
2024-09-01 21:40:34 +0530

    Time: 2 ms (99.96%) | Memory: 58.4 MB (10.66%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if(nums.length<=k)
+        {
+            k=nums.length-1;
+        }
+        int count= 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            int j=i+1;
+            while(j<nums.length&& j<=i+k)
+            {
+                if(nums[i]==nums[j])
+                {
+                    return true;
+                }
+                j++;
+                count++;
+                if(count>9999)
+                {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+}
