a5db6146b79f65a4d84cc8aca776f06c3c16daf3
HARDIK ARORA <122469904+barbaria888@users.noreply.github.com>
2024-09-03 21:37:28 +0530

    Time: 72 ms (14.29%) | Memory: 49.7 MB (90.76%) - LeetSync

---
+++ 2741-function-composition/function-composition.js
@@ -0,0 +1,31 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    if(functions.length==0)
+    {
+        return function(x)
+        {
+            return x;
+        }
+    }
+
+    return function(x) {
+        {
+            var result= functions[functions.length-1](x);
+            for(let i= functions.length-2;i>=0;i--)
+            {
+                result=functions[i](result);
+            }
+            
+        }
+        return result;
+    
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
