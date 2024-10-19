ca42d0090787934eb06653b1d72fd9191d45e81f
HARDIK ARORA <122469904+barbaria888@users.noreply.github.com>
2024-10-19 17:13:35 +0530

    Time: 2 ms (32.08%) | Memory: 48.5 MB (46.75%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -1,42 +1,60 @@
 /**
  * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-public class Solution {
-    
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode a= headA;
-        ListNode b= headB;
-        while(a!=b)    
-        {
-
-            if(a==null &&b==null)
-            {
-                return null;
-            }
-            else if(a==null)
-            {
-                a=headB;
-            }
-            
-            else if(b==null)
-            {
-                b=headA;
-            }
-            else
-            {
-                a=a.next;
-                b=b.next;
-            }
-               
-        }
-        return a;
+ 
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
     }
+}
+*/
+public class Solution {
+    public static int size(ListNode head) {
+    ListNode curr = head;
+    int size = 0;
+    while (curr != null) 
+    {
+        size++;
+        curr = curr.next;
+    }
+    return size;
+    }
+
+public ListNode getIntersectionNode(ListNode headA, ListNode headB) {                      
+        if (headA == null || headB == null) 
+        {
+          return null;
+        }
+        int n1 = size(headA);
+        int n2 = size(headB);
+        ListNode ptrA = headA;
+        ListNode ptrB = headB;
+        
+        if (n1 > n2) {
+            int diff = n1 - n2;
+            for (int i = 0; i < diff; i++) {
+                ptrA = ptrA.next;
+            }
+        } else if (n1 < n2) {
+            int diff = n2 - n1;
+            for (int i = 0; i < diff; i++) {
+                ptrB = ptrB.next;
+            }
+        }
+
+
+        while (ptrA != null && ptrB != null) 
+        {
+            if (ptrA == ptrB) 
+            {
+                return ptrA;
+            }
+            ptrA = ptrA.next;
+            ptrB = ptrB.next;
+        }
+
+        return null; 
+    }   
 }
