fe911fe822a4ef9fd5f8c21f10b9ab106548fb3c
HARDIK ARORA <122469904+barbaria888@users.noreply.github.com>
2025-01-09 23:54:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (34.06%) - LeetSync

---
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if(root==null)       
+        {
+        return ;
+        }
+        TreeNode left=root.left;
+        TreeNode right=root.right;
+        root.left=null;
+        flatten(left);
+        flatten(right);
+        root.right=left;    
+        TreeNode temp=left;
+        while(temp!=null&&temp.right!=null)
+        {
+            temp=temp.right;
+        }
+        if(temp!=null)
+        {
+            temp.right=right;
+        }
+        else
+        {
+            root.right=right;
+        }
+        return;
+    }
+}
