0f9b21b47b20d43dde2b6facdb57adfe04fd94c2
HARDIK ARORA <122469904+barbaria888@users.noreply.github.com>
2025-02-02 23:53:53 +0530

    Time: 104 ms (44.95%) | Memory: 49.3 MB (59.17%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -11,11 +11,12 @@ class Solution {
         {
             
             if(primes[i])
-            for(int j=i*i;j<n ;j+=i)
             {
-                primes[j]=false;
+                for(int j=i*i;j<n ;j+=i)
+                {
+                    primes[j]=false;
+                }
             }
-            
         }
         for(int i=2;i<n;i++)
         {
