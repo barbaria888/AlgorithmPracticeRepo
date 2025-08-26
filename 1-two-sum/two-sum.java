de163548bf01f1306c2e5d28bcb20f628722733a
HARDIK ARORA <122469904+barbaria888@users.noreply.github.com>
2025-08-26 09:12:41 +0530

    Time: 2 ms (98.88%) | Memory: 45.5 MB (5.09%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,8 +1,6 @@
-import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap <Integer,Integer>hm=new HashMap<>();
-        
+    HashMap <Integer,Integer>hm=new HashMap<>();
         for(int i=0;i<nums.length;i++)
         {
             int complement= target-nums[i];
